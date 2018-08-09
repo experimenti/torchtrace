194733351aa93212771c98c3d0f70ad126eea6e5
John <john.douglas@mycostech.com>
2018-08-09 22:27:04 +0700

    batch array load

--- harperdb.py
+++ harperdb.py
@@ -6,6 +6,10 @@ import json
 import urllib.request
 import base64
 import requests
+import sys
+import os
+import time
+import uuid
 
 
 def frameToExcel(self, narray, label):
@@ -84,18 +88,103 @@ def postToHarper(data, url="http://localhost:9925", user='harperdb', password='h
     response = requests.request(
         "POST", url, data=json.dumps(data), headers=headers)
 
+    print(response.text)
+
     return response.json()
 
 
-def tensortoHarperDB():
-    print("Building Frame")
+def insertTensor():
 
-    D_in, D_out = 64, 100
+    D_in, D_out = 10, 10
 
     # Create random input and output data
     x = np.random.randn(D_in, D_out)
 
+    size = sys.getsizeof(x)
 
-# createSchema()
-# createTable('x')
-tensortoHarperDB()
+    data_x = [
+        {
+            "id": 4,
+            "time": time.time(),
+            "size": size,
+            "tensor": x.tolist()
+        }
+    ]
+
+    op_dict = {
+        'operation': 'insert',
+        'schema': 'torchtrace',
+        'table': 'x',
+        'records': data_x
+    }
+
+    print(postToHarper(op_dict))
+
+
+def getDirectorySize():
+    path = '/home/john/hdb'
+    folder = sum([sum(map(lambda fname: os.path.getsize(os.path.join(
+        directory, fname)), files)) for directory, folders, files in os.walk(path)])
+    MB = 1024*1024.0
+
+    print("{0}".format(folder/MB))
+
+    return folder/MB
+
+
+payload = "{\n\"operation\":\"read_log\",\n\"limit\":1000,\n\"start\":0,\n\"from\":\"2017-07-10\",\n\"until\":\"2019-07-11\",\n\"order\":\"desc\"\n}"
+
+
+def exportTableToCSV():
+
+    query = "SELECT * FROM {0}".format("torchtrace.x")
+
+    print(query)
+
+    op_dict = {
+        "operation": "export_local",
+        "format": "csv",
+        #"path":  "/mnt/c/Users/MYCOS-PC/code/exports/batchperf.csv",
+        "path":  "/home/batchperf.csv",
+        "search_operation": {
+            "operation": "sql",
+                "sql": query 
+        }
+    }
+
+    postToHarper(op_dict)
+
+def batchInsertTensors(iterations=1):
+
+    for k in range(0, iterations):
+
+        D_in, D_out = 10 + k, 10 + k
+
+        # Create random input and output data
+        x = np.random.randn(D_in, D_out)
+
+        size = getDirectorySize()
+
+        data = [
+            {
+                "id": uuid.uuid4().hex,
+                "time": time.time(),
+                "preinsert_size": size,
+                "array_size_bytes": x.size * x.itemsize,
+                "tensor": x.tolist()
+            }
+        ]
+
+        op_dict = {
+            'operation': 'insert',
+            'schema': 'torchtrace',
+            'table': 'x',
+            'records': data
+        }
+
+        postToHarper(op_dict)
+
+
+# insertTensor()
+batchInsertTensors()
+exportTableToCSV()
